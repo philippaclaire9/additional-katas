3a1e9e556c5fc62a4d1b3404bc873200856722b1
Philippa <philippa.claire9@gmail.com>
2020-06-15 13:23:07 +0100

    begin findConnectFourWinner with losing boards

--- __tests__/findConnectFourWinner.test.js
+++ __tests__/findConnectFourWinner.test.js
@@ -1,7 +1,52 @@
-const { findConnectFourWinner } = require('../findConnectFourWinner')
+const { findConnectFourWinner } = require('../findConnectFourWinner');
 
 describe('findConnectFourWinner()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+  xit('Returns Please provide a 7 * 6 matrix if no matrix provided', () => {
+    expect(findConnectFourWinner([])).toEqual('Please provide a 7 * 6 matrix');
+  });
+  xit('Returns Please provide a 7 * 6 matrix matrix dimensions are incorrect', () => {
+    expect(
+      findConnectFourWinner([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual('Please provide a 7 * 6 matrix');
+    expect(
+      findConnectFourWinner([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual('Please provide a 7 * 6 matrix');
+  });
+  xit('Returns false when no winner when board is null', () => {
+    expect(
+      findConnectFourWinner([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual(false);
+  });
+  it('Returns false when no winner', () => {
+    expect(
+      findConnectFourWinner([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, 'o', null, null, null, null],
+        [null, null, 'x', null, null, null, null],
+        [null, 'x', 'o', 'x', null, null, null],
+        [null, 'x', 'o', 'x', null, null, null],
+      ])
+    ).toEqual(false);
+  });
+});
--- findConnectFourWinner.js
+++ findConnectFourWinner.js
@@ -1,6 +1,55 @@
 // Please do not change the name of this function
 const findConnectFourWinner = (board) => {
-  // Your code here
+  if (!board.length) return 'Please provide a 7 * 6 matrix';
+  if (board.length !== 6) return 'Please provide a 7 * 6 matrix';
+  let invalidBoard = false;
+
+  board.forEach((array) => {
+    if (array.length !== 7) {
+      invalidBoard = true;
+    }
+  });
+  if (invalidBoard) return 'Please provide a 7 * 6 matrix';
+
+  // console.log(
+  //   board.length,
+  //   board.length !== 6,
+  //   !board.every((array) => array.length === 7)
+  // );
+  // if (
+  //   !board.length ||
+  //   board.length !== 6 ||
+  //   !board.every((array) => {
+  //     array.length === 7;
+  //   })
+  // )
+  //   return 'Please provide a 7 * 6 matrix';
+
+  let nullCount = 0;
+  let arrayIndex = 0;
+
+  let xIndex = 0;
+  let oIndex = 0;
+
+  const isX = (value) => value === 'x';
+  const isO = (value) => value === 'o';
+
+  board.forEach((array) => {
+    if (board.indexOf(array) !== 0) {
+      arrayIndex++;
+      if (!array.includes('x') && !array.includes('o')) {
+        nullCount++;
+      } else {
+        xIndex = array.findIndex(isX);
+        oIndex = array.findIndex(isO);
+        console.log('x >>>>', xIndex, oIndex, '<<<< o');
+      }
+    }
+  });
+
+  if (nullCount === 5) return false;
+
+  board.forEach;
 };
 
 module.exports = { findConnectFourWinner };
