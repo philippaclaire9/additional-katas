2282ae6e8ff3c51144f0afe9bfcdc6593c0a2b0b
Philippa <philippa.claire9@gmail.com>
2020-09-07 13:13:24 +0100

    recap checkDiagonalWin to flex kata muscles

---
+++ __tests__/checkDiagWin.test.js
@@ -0,0 +1,76 @@
+const { checkDiagWin } = require('../checkDiagWin');
+
+describe('checkDiagWin()', () => {
+  it('returns Please provide a 7*6 matrix when no matrix is provided', () => {
+    expect(checkDiagWin([])).toEqual('Please provide a 7*6 matrix');
+  });
+  it('returns Please provide 7*6 matrix when matrix is incorrect size', () => {
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual('Please provide a 7*6 matrix');
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual('Please provide a 7*6 matrix');
+  });
+  it('returns false when no diagonal winner (no counters)', () => {
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+      ])
+    ).toEqual(false);
+  });
+  it('returns false when no diagonal winner (horizontal counters)', () => {
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, 'x', 'x', 'x', 'x', null, null],
+      ])
+    ).toEqual(false);
+  });
+  it('returns false when no diagonal winner (vertical counters)', () => {
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, 'o', null, null, null, null, null],
+        [null, 'o', null, null, null, null, null],
+        [null, 'o', null, null, null, null, null],
+        [null, 'o', null, null, null, null, null],
+      ])
+    ).toEqual(false);
+  });
+  it('returns true when diagonal winner', () => {
+    expect(
+      checkDiagWin([
+        [null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null],
+        [null, null, null, 'x', null, null, null],
+        [null, null, 'x', null, null, null, null],
+        [null, 'x', null, null, null, null, null],
+        ['x', null, null, null, null, null, null],
+      ])
+    ).toEqual(true);
+  });
+});
---
+++ checkDiagWin.js
@@ -0,0 +1,82 @@
+const checkDiagWin = (board) => {
+  if (!board.length) return 'Please provide a 7*6 matrix';
+  let rowCounter = 0;
+  board.forEach((row) => {
+    rowCounter++;
+  });
+  if (rowCounter != 6) {
+    return 'Please provide a 7*6 matrix';
+  }
+
+  let spaceCounter = 0;
+  let incorrectSpaces = false;
+
+  board.forEach((row) => {
+    spaceCounter = 0;
+    row.forEach((space) => {
+      spaceCounter++;
+    });
+    if (spaceCounter !== 7) {
+      incorrectSpaces = true;
+    }
+  });
+  if (incorrectSpaces) return 'Please provide a 7*6 matrix';
+
+  let currentPlayer = '';
+  let spaceIndex = 0;
+  let horizontalSpaceIndex = 0;
+  let verticalSpaceIndex = 0;
+  let rowIndex = 0;
+  let horizontalCounters = 1;
+  let verticalCounters = 0;
+
+  board.forEach((row) => {
+    rowIndex++;
+    if (!currentPlayer) {
+      row.forEach((space) => {
+        if (space) {
+          currentPlayer = space;
+          spaceIndex = row.indexOf(space);
+          horizontalSpaceIndex = row.indexOf(space);
+          verticalSpaceIndex = row.indexOf(space);
+          rowIndex = board.indexOf(row);
+        }
+      });
+    }
+
+    if (currentPlayer) {
+      row.forEach((space) => {
+        if (board[rowIndex][spaceIndex + 1] === currentPlayer) {
+          horizontalCounters++;
+          spaceIndex++;
+        }
+      });
+    }
+
+    if (currentPlayer) {
+      if (rowIndex <= 4) {
+        //console.log(board[rowIndex + 1][verticalSpaceIndex]);
+        if (board[rowIndex + 1][verticalSpaceIndex] === currentPlayer) {
+          verticalCounters++;
+          //   console.log(rowIndex, verticalCounters);
+        }
+      }
+      if (rowIndex === 5) {
+        if (board[rowIndex][verticalSpaceIndex] === currentPlayer) {
+          verticalCounters++;
+          //   console.log(rowIndex, verticalCounters);
+        }
+      }
+    }
+  });
+
+  if (verticalCounters === 4) {
+    return false;
+  }
+  if (horizontalCounters === 4) {
+    return false;
+  }
+  if (!currentPlayer) return false;
+};
+
+module.exports = { checkDiagWin };
